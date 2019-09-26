b23be55cef912f7f6ad1801e41ec66eb58fa1eda
2114550 <2114550@talnet.nl>
2019-09-26 11:29:39 +0200

    Uitwerking van de exercise

--- calculator.php
+++ calculator.php
@@ -1,13 +1,19 @@
 <?php
 echo "Wat wil je doen?(+,-,%)".PHP_EOL;
     $var = readline();
-    if ($var != "+"."-"."%")
-        echo "Niet geldig";
-    exit;
-echo "Eerste getal?".PHP_EOL;
-    $a = readline();
-echo "Tweede getal?".PHP_EOL;
-    $b = readline();
+    if ($var == "+" or
+        $var == "-" or
+        $var == "%")
+    {
+        echo "Eerste getal?".PHP_EOL;
+        $a = readline();
+        echo "Tweede getal?".PHP_EOL;
+        $b = readline();
+    }
+    else
+{
+    echo "Antwoord ongeldig".PHP_EOL;
+}
 if ($var == "+")
 {
     $c = $a + $b;
